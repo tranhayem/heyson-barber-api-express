1d446a20306771376cf2bfab0e51607cf48e0e6c
tranhayem <mailcuathy@gmail.com>
2024-11-25 03:19:54 +0700

    chore: initial project setup with express@4.21.1

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const hostname = "localhost";
+const port = 2728;
+
+app.get("/", (request, response) => {
+  response.send("Hello World!");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`HeySon server đang chạy trên: http://${hostname}:${port}`);
+});
